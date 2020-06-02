6594d5e674e1896f935171582e1d757b8a5cae24
Roman <54954154+DanteRive@users.noreply.github.com>
2020-06-02 11:01:29 +0400

    end

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <div class="footer">
 <div class="footer__logo">DRIVEBET</div>
 <nav class="foot_nav">
-                <a class="foot_nav__link" name="about" href="#">ООО "Drivebet"</a>
-                <a class="foot_nav__link" name="service" href="#">Ул.Пушкина д-41</a>
-                <a class="foot_nav__link" name="blog" href="#">8(800)555-35-35</a>
+                <a class="foot_nav__link" name="about" href="mailto:karog7789@gmail.com">ООО "Drivebet"</a>
+                <a class="foot_nav__link" name="service" href="https://goo.gl/maps/XqjJ55fujqYgBL1n9" target="_blank">Ул.Пушкина д-41</a>
+                <a class="foot_nav__link" name="blog" href="tel:+78005553535">8(800)555-35-35</a>
             </nav>
             <br><nav class="foot_nav">
-                <a class="foot_nav__link" name="about" href="#"><img src="images/Facebook.png" alt=""></a>
-                <a class="foot_nav__link" name="about" href="#"><img src="images/Google +.png" alt=""></a>
-                <a class="foot_nav__link" name="about" href="#"><img src="images/INST.png" alt=""></a>
-                <a class="foot_nav__link" name="blog" href="#"><img src="images/Twitter.png" alt=""></a>
-                <a class="foot_nav__link" name="blog" href="#"><img src="images/vk.png" alt=""></a>
+                <a class="foot_nav__link" name="about" href="https://www.facebook.com" target="_blank"><img src="images/Facebook.png" alt=""></a>
+                <a class="foot_nav__link" name="about" href="https://g.co/kgs/SkonP7" target="_blank"><img src="images/Google +.png" alt=""></a>
+                <a class="foot_nav__link" name="about" href="https://www.instagram.com" target="_blank"><img src="images/INST.png" alt=""></a>
+                <a class="foot_nav__link" name="blog" href="https://twitter.com" target="_blank"><img src="images/Twitter.png" alt=""></a>
+                <a class="foot_nav__link" name="blog" href="https://vk.com" target="_blank"><img src="images/vk.png" alt=""></a>
             </nav>
             </div>
     </div>
